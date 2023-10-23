63959b035bc05ed488fbab9a324e04cc0eb60b11
Eugene T <tyanbz@gmail.com>
2023-10-23 20:15:55 +0600

    architecture init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <span style={{color: 'green'}}>This is span with styled comps</span>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/comps/Button.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const Button = () => {
+  return (
+    <React.Fragment></React.Fragment>
+  );
+};
+
+export default Button;
